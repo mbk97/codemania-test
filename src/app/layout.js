4b4ab721a9a60f6379c95ac7ef74f86d4bb1d899
mubarak.muhammed <mubarak.muhammed@wemabank.com>
2023-11-23 21:44:12 +0100

    product api integration completion

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,8 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/redux/provider";
 import StyledComponentsRegistry from "./registry";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +14,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToastContainer />
         <StyledComponentsRegistry>
           <Providers>{children}</Providers>
         </StyledComponentsRegistry>
--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,6 +1,14 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { ProductCardContainer, ProductsCardWrapper, Title } from "./style";
+import {
+  ProductCardContainer,
+  ProductsCardWrapper,
+  Title,
+  PaginationWrapper,
+  PaginationBtn,
+  FlexWrapper,
+  NoProductText,
+} from "./style";
 import { useDispatch, useSelector } from "react-redux";
 import Navbar from "../navbar/Navbar";
 import CustomModal from "../customModal/CustomModal";
@@ -9,13 +17,24 @@ import { fetchAllProducts } from "@/services";
 import { setProducts } from "@/redux/productSlice";
 import ProductCard from "../productCard/ProductCard";
 import Loader from "../loader/Loader";
-import { toast } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ReactPaginate from "react-paginate";
+import { categoriesData } from "@/utils/data";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
   const [productDetail, setProductDetail] = useState({});
+  const [filterItem, setFilterItem] = useState("");
   const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const productsPerPage = 10;
+
+  const handleChange = (e) => {
+    setFilterItem(e.target.value);
+  };
 
   const handleOpenMenu = (product) => {
     setOpen(true);
@@ -25,51 +44,90 @@ const Dashboard = () => {
     setOpen(false);
   };
 
-  const getAllProducts = async () => {
-    try {
-      setLoading(true);
-      const data = await fetchAllProducts();
-      if (data) {
-        toast.success("Successful", {
-          theme: "colored",
-        });
-        dispatch(setProducts(data));
-      }
-      setLoading(false);
-    } catch (e) {
-      toast.error(e.message, {
-        theme: "colored",
-      });
-      setLoading(false);
-    }
-  };
+  const productsData = useSelector((state) => state.product.productsData);
 
   useEffect(() => {
-    getAllProducts();
-  }, []);
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const data = await fetchAllProducts();
+        if (data) {
+          dispatch(setProducts(data));
+          setFilteredProducts(
+            filterItem === ""
+              ? data?.products
+              : data?.products.filter(
+                  (product) => product.category === filterItem,
+                ),
+          );
+        }
+      } catch (e) {
+        toast.error(e.message, {
+          theme: "colored",
+        });
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  const productsData = useSelector((state) => state.product.productsData);
+    fetchData();
+  }, [filterItem]);
   return (
     <React.Fragment>
+      <ToastContainer />
       <Navbar />
-      <Title>Products</Title>
-      {loading && <Loader />}
-      {!loading && productsData?.products?.length !== 0 ? (
+      <FlexWrapper>
+        <Title>Products</Title>
+        <select onChange={handleChange}>
+          <option selected disabled value="phone" key="">
+            Filter Products
+          </option>
+          {categoriesData.map(({ id, name, label }) => {
+            return (
+              <option value={name} key={id}>
+                {label}
+              </option>
+            );
+          })}
+        </select>
+      </FlexWrapper>
+      {loading ? (
+        <Loader />
+      ) : filteredProducts?.length !== 0 ? (
         <ProductCardContainer>
           <ProductsCardWrapper>
-            {productsData?.products?.map((product) => {
-              return (
+            {filteredProducts
+              ?.slice(
+                currentPage * productsPerPage,
+                (currentPage + 1) * productsPerPage,
+              )
+              .map((product) => (
                 <ProductCard
                   key={product.id}
                   product={product}
                   handleOpenMenu={handleOpenMenu}
                 />
-              );
-            })}
+              ))}
           </ProductsCardWrapper>
+          <PaginationWrapper>
+            <ReactPaginate
+              previousLabel={<PaginationBtn>{"<"}</PaginationBtn>}
+              nextLabel={<PaginationBtn>{">"}</PaginationBtn>}
+              breakLabel={"..."}
+              breakClassName={"break-me"}
+              pageCount={Math.ceil(
+                productsData?.products?.length / productsPerPage,
+              )}
+              marginPagesDisplayed={2}
+              pageRangeDisplayed={5}
+              onPageChange={({ selected }) => setCurrentPage(selected)}
+              containerClassName={"pagination"}
+              activeClassName={"active"}
+            />
+          </PaginationWrapper>
         </ProductCardContainer>
       ) : (
-        <p>No product found</p>
+        <NoProductText>No products found</NoProductText>
       )}
       <CustomModal open={open} handleClose={handleCloseMenu}>
         <ProductDetailsModal productDetail={productDetail} />
